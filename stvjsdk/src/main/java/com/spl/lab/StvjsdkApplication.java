ab340dcdd144785ce98d53fd091ae78fc0625504
kisimita <caothucolieu@yahoo.com>
2018-11-14 10:01:53 +0700

    update code

--- stvjsdk/src/main/java/com/spl/lab/StvjsdkApplication.java
+++ stvjsdk/src/main/java/com/spl/lab/StvjsdkApplication.java
@@ -8,5 +8,7 @@ public class StvjsdkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StvjsdkApplication.class, args);
+		Service.WakeOnWirelessLan("d8:e0:e1:92:36:83");
+//		Service.WakeOnWirelessLan("CC:6E:A4:D0:B8:CC");
 	}
 }
